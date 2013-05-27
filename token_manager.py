4233872d8a17b0ad932d8c606147da693c98f2a0
Diogo Dutra <diogodutra@nyxk.com.br>
2013-05-27 19:43:08 -0300

    adicionado um auto refresh do token para cada acesso no cliente.

--- token_manager.py
+++ token_manager.py
@@ -5,18 +5,21 @@ from threading import Timer
 
 class TokenManager(object):
     def __init__(self, expire_time=None):
-        self.clients = {}
+        self._clients = {}
         self._timer = None
         self._is_running = True
         if expire_time:
             self._expire_time = expire_time
             self._expire_tokens()
 
+    def __del__(self):
+        self.stop()
+
     def _expire_tokens(self):
-        for key, value in self.clients.items():
+        for key, value in self._clients.items():
             time = value['time']
             if (datetime.now() - time) >= timedelta(0, self._expire_time*60):
-                self.clients.pop(key)
+                self._clients.pop(key)
         if self._is_running:
             self._timer = Timer(self._expire_time*60, self._expire_tokens)
             self._timer.start()
@@ -27,7 +30,7 @@ class TokenManager(object):
             self._timer.cancel()
 
     def expire_token(self, token, user_agent, remote_addr):
-        if self.clients.pop((token, user_agent_header, remote_addr), False):
+        if self._clients.pop((token, user_agent_header, remote_addr), False):
             return True
         else:
             return False
@@ -39,8 +42,15 @@ class TokenManager(object):
                   'addr': remote_addr,
                   'time': now,
                   'user': user}
-        self.clients[token] = client
+        self._clients[token] = client
         return token
 
+    def get_client(self, token):
+        if self._clients.has_key(token):
+            self._clients[token]['time'] = datetime.now()
+            return self._clients[token]
+        else:
+            return False
+
     def validate(self, token, user_agent, remote_addr):
-        return self.clients.has_key((token, user_agent_header, remote_addr))
+        return self._clients.has_key((token, user_agent_header, remote_addr))
